279bd4a26ca226d87542511b7e4cf956ba269a45
zx-afk-dev <daiipratama064@gmail.com>
2026-09-24 12:49:26 +0700

    Update index.js

--- pages/api/admin/redeem-codes/index.js
+++ pages/api/admin/redeem-codes/index.js
@@ -1,2 +1,5 @@
 import { createCrudHandler } from '../../../../lib/firestoreCrud';
-export default createCrudHandler('redeemCodes', { idPrefix: 'RDM', idLength: 8 });
+export default createCrudHandler('redeemCodes', {
+  idPrefix: (data) => (data.type === 'vip' ? 'VIP' : data.type === 'dev' ? 'DEV' : 'RDM'),
+  idLength: 8,
+});
